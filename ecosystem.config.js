6c889d7851e77fa7ab64ad788c21e859a2e85e80
Nuno Cardoso <nuno_kisc@hotmail.com>
2026-04-06 18:06:24 +0100

    fix: correct working directory path in ecosystem configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "festasearraiais-submit-new",
       script: "node_modules/.bin/next",
       args: "start",
-      cwd: "/var/www/html/festasearraiais_submit_new",
+      cwd: "/var/www/html/festasearraiais/festasearraiais_submit_new",
       env_production: {
         NODE_ENV: "production",
         PORT: 3009
