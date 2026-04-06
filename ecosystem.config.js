3256b393004ffa0ca5c1e316608edeb0951ddd50
Nuno Cardoso <nuno_kisc@hotmail.com>
2026-04-06 18:01:49 +0100

    fix: update production environment configuration to use correct PORT and remove unused URLs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,10 +7,7 @@ module.exports = {
       cwd: "/var/www/html/festasearraiais_submit_new",
       env_production: {
         NODE_ENV: "production",
-        PORT: 3002,
-        NEXT_PUBLIC_BACKOFFICE_API_URL: "https://backoffice-api.festasearraiais.pt",
-        NEXT_PUBLIC_MAIN_SITE_URL: "https://festasearraiais.pt",
-        NEXT_PUBLIC_APP_URL: "https://adicionar.festasearraiais.pt",
+        PORT: 3009
       },
     },
   ],
